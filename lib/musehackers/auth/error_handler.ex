8f332c2d0a1f130d673b69471ebed11053b60add
Peter Rudenko <peter.rudenko@gmail.com>
2018-03-10 15:35:34 +0400

    Speedup camel case transform

--- lib/musehackers/auth/error_handler.ex
+++ lib/musehackers/auth/error_handler.ex
@@ -3,7 +3,7 @@ defmodule Musehackers.Auth.ErrorHandler do
   @moduledoc false
 
   def auth_error(conn, {type, _reason}, _opts) do
-    body = Poison.encode!(%{message: to_string(type)})
+    body = Jason.encode!(%{message: to_string(type)})
     send_resp(conn, 401, body)
   end
 end
