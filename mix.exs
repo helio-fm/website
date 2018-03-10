4e83356e15134c37ffb659266a7a459194c351c6
Peter Rudenko <peter.rudenko@gmail.com>
2018-03-10 11:09:31 +0400

    Bump Elixir version dependency to 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Musehackers.Mixfile do
     [
       app: :musehackers,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
@@ -44,7 +44,7 @@ defmodule Musehackers.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.3.0"},
       {:postgrex, ">= 0.13.3"},
