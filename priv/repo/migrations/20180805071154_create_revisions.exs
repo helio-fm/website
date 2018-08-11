bedd2947a451490493aff2d26c4295623bb93800
Peter Rudenko <peter.rudenko@gmail.com>
2018-08-11 10:48:35 +0400

    Add migrations for project and revision model

---
+++ priv/repo/migrations/20180805071154_create_revisions.exs
@@ -0,0 +1,20 @@
+defmodule Musehackers.Repo.Migrations.CreateRevisions do
+  use Ecto.Migration
+
+  def change do
+    create table(:revisions, primary_key: false) do
+      add :id, :string, primary_key: true, null: false
+      add :message, :string, null: false
+      add :hash, :string, null: false
+      add :data, :map, null: false
+      add :project_id, references(:projects, on_delete: :nothing, column: :id, type: :string), null: false
+      add :parent_revision_id, references(:revisions, on_delete: :nothing, column: :id, type: :string), null: true
+
+      timestamps()
+    end
+
+    create index(:revisions, [:project_id])
+    create index(:revisions, [:parent_revision_id])
+    create unique_index(:revisions, [:id, :project_id], name: :revisions_unique_id_per_project)
+  end
+end
